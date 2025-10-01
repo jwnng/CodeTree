b1f7ecffd7dd66c7c21367c162570e68000656a8
jwnng <rlawldnd905@naver.com>
2025-10-01 13:56:02 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251001/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        scanner sc = new Scanner(System.in);
+       String userinput = sc.next(); 
+        int a = sc.nextInt();
+        System.out.println("a");
+    }
+}
