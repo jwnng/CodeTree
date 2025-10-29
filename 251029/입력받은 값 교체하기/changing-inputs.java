0439b2fc505b7859962c6743546e8563051a24ef
jwnng <rlawldnd905@naver.com>
2025-10-29 14:28:51 +0900

    Title: 입력받은 값 교체하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251029/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.printf("%d %d\n",a,b);
+        // Please write your code here.
+    }
+}
