cd21e4b7a4e9ab3a334dbc9f9019f8a7e4f69651
jwnng <rlawldnd905@naver.com>
2025-12-03 14:26:25 +0900

    Title: 합과 평균의 차, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251203/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let avg = Math.floor((a+b+c)/3);
+
+console.log(a+b+c);
+console.log(avg);
+console.log((a+b+c) - avg);
