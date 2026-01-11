e07ead498ded2686224fe7670351e2323a561b1a
jwnng <rlawldnd905@naver.com>
2026-01-11 21:43:10 +0900

    Title: 100점 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260111/100점 도달하기/reach-100-point.py
@@ -0,0 +1,12 @@
+n = int(input())
+for n in range(n,101,1):
+    if n >= 90:
+        print('A',end=' ')
+    elif n >= 80:
+        print('B',end=' ')
+    elif n >= 70:
+        print('C',end=' ')
+    elif n >= 60:
+        print('D',end=' ')
+    else :
+        print('F',end=' ')
