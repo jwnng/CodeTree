cab4f2b29f321c088b35fb1c9389a00168bb568a
jwnng <rlawldnd905@naver.com>
2026-01-20 15:49:35 +0900

    Title: 별표 출력하기 12, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260120/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i == 1:
+            print("*",end=" ")
+        elif i != 1 and j % 2 == 0 and i <= j:
+            print("*",end=" ")
+        else :
+            print(" ",end=" ")
+    print()
