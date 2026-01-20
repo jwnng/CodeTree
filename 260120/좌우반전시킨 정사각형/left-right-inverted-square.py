e1a9c0241405f62cb8558530de6f4ea54b8a0d6d
jwnng <rlawldnd905@naver.com>
2026-01-20 16:58:12 +0900

    Title: 좌우반전시킨 정사각형, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260120/좌우반전시킨 정사각형/left-right-inverted-square.py
+++ 260120/좌우반전시킨 정사각형/left-right-inverted-square.py
@@ -1,12 +1,7 @@
 n = int(input())
-m = [ [0 for i in range(n)] for j in range(n)]
 
-for i in range(n):
-    for j in range(n):
-        m[i][j] = (i+1)*(j+1)
-
-for i in range(n):
-    for j in range(n):
-        print(m[i][n-j-1],end=" ")
+for i in range(1,n+1):
+    for j in range(n,0,-1):
+        print(i*j,end=" ")
     print()
         
