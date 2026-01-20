4a5152ba55ff4aa7cbab4144594847d6b5ee9de2
jwnng <rlawldnd905@naver.com>
2026-01-20 19:22:27 +0900

    Title: 상하로 반복하며 출력, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260120/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 260120/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -1,14 +1,10 @@
 n = int(input())
-m = [[0 for i in range(n)]for j in range(n)]
+
 for i in range(n):
     for j in range(n):
-        if i % 2 == 0:
-            m[j][i] = j+1
+        if j % 2 == 0:
+            cnt = i+1
         else :
-            m[j][i] = n-j
-
-
-for i in range(n):
-    for j in range(n):
-        print(m[i][j],end="")
+            cnt = n-i
+        print(cnt,end="")
     print()
