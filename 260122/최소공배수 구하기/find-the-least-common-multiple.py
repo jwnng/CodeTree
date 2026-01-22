981740d5bfa76d578467ad510381c949c0238d36
jwnng <rlawldnd905@naver.com>
2026-01-22 20:29:17 +0900

    Title: 최소공배수 구하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260122/최소공배수 구하기/find-the-least-common-multiple.py
+++ 260122/최소공배수 구하기/find-the-least-common-multiple.py
@@ -3,13 +3,13 @@ n, m = map(int, input().split())
 # Please write your code here.
 def min_mul(n,m):
     mul = []
-
     for i in range(1,n+1):
         for j in range(1,m+1):
             if n * i == m * j:
-                    mul.append(n * i)
-                    break
-                
+                mul.append(n * i)
+                break
+    if mul == []:
+        mul.append(n*m)
     print(mul[0])
 
 min_mul(n,m)
